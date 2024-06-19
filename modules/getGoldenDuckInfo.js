fde3f348c03103fe8fb19f0409f142e8dfc66943
kenIT1 <dinhcutequaa@gmail.com>
2024-06-20 00:52:46 +0700

    🔥 Restore V4

--- modules/getGoldenDuckInfo.js
+++ modules/getGoldenDuckInfo.js
@@ -1,6 +1,7 @@
 const getAction = require("../actions/get");
 const sleep = require("./sleep");
 const config = require("../config.json");
+const addLog = require("./addLog");
 
 async function getGoldenDuckInfo(token, ua) {
   let retry = 0;
@@ -20,37 +21,52 @@ async function getGoldenDuckInfoInternal(token, ua) {
   try {
     const response = await getAction(token, "golden-duck/info", ua);
     // console.log(data);
-    return response.data.data;
+    return response.data;
   } catch (error) {
-    console.log("getGoldenDuckInfo error");
     if (error.response) {
-      // console.log(error.response.data);
-      console.log("status", error.response.status);
-      // console.log("data", error.response.data);
       const status = error.response.status;
-      // console.log(error.response.headers);
-      if (status === 503 || status === 502 || status === 504) {
-        console.log("Mat ket noi, tu dong ket noi sau 30s");
-        await sleep(30);
+
+      if (status >= 500) {
+        console.log("Lost connect, auto connect after 5s, retry to die");
+        addLog(`getGoldenDuckInfoInternal error ${status}`, "error");
+        await sleep(5);
         return null;
       } else if (status === 401) {
         console.log(`\nToken loi hoac het han roi\n`);
+        addLog(
+          `getGoldenDuckInfoInternal error Token loi hoac het han roi`,
+          "error"
+        );
         process.exit(1);
       } else if (status === 400) {
-        console.log("data", error.response.data);
-        await sleep(10);
-        return null;
+        addLog(
+          `getGoldenDuckInfoInternal error ${error.response.data.error_code}`,
+          "error"
+        );
+        return error.response.data;
       } else {
-        await sleep(5);
+        console.log("Lost connect, auto connect after 3s, retry to die");
+        addLog(`getGoldenDuckInfoInternal error ${status} undefined`, "error");
+        await sleep(3);
         return null;
       }
     } else if (error.request) {
       console.log("request", error.request);
+      console.log("Lost connect, auto connect after 3s, retry to die");
+      addLog(
+        `getGoldenDuckInfoInternal error request ${error.request}`,
+        "error"
+      );
+      await sleep(3);
+      return null;
     } else {
       console.log("error", error.message);
+      console.log("Lost connect, auto connect after 3s, retry to die");
+      addLog(`getGoldenDuckInfoInternal error ${error.message}`, "error");
+      await sleep(3);
+      return null;
     }
   }
-  return null;
 }
 
 module.exports = getGoldenDuckInfo;
